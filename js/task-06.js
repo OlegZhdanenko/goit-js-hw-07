5fc06f14452f6afc29cc2e7ce56edab5b5cbb98f
OlegZhdanenko <125878669+OlegZhdanenko@users.noreply.github.com>
2023-12-04 15:25:09 +0300

    sum

--- js/task-06.js
+++ js/task-06.js
@@ -6,43 +6,38 @@ function getRandomHexColor() {
 
 
 
-const controls = document.querySelector("#controls");
+const input = document.querySelector("input");
 const createBox = document.querySelector("button[data-create]");
 const destroyBox = document.querySelector("button[data-destroy]");
 const boxes = document.querySelector("#boxes")
 
-controls.addEventListener("input", onInput);
-createBox.addEventListener("click", createBoxes);
-destroyBox.addEventListener("click", destroyBoxes);
+createBox.addEventListener("click", onCreateBtn);
+destroyBox.addEventListener("click", onDestroyBtn);
 
-let sizeBox = 0;
-let numberOfInput = 0;;
-let step = 30;
 
-function onInput(event) {
-  let evt = event.target.value;
-  createBoxes(evt);
- 
+function onCreateBtn(event) {
+  const amount = input.value;
+  createBoxes(amount);
 }
 
-
-
+function onDestroyBtn(event) {
+destroyBoxes(event)
+}
 
 function createBoxes(amount) {
-  for (let i = 0; i < amount; i+=1) {
+  let newBoxes = [];
+  let boxSizing = 30;
+
+  for (let i = 0; i < amount; i += 1) {
     const divBox = document.createElement("div")
     divBox.classList.add("box-list")
-  divBox.style.width = "30px";
-  divBox.style.height = "30px";
     divBox.style.backgroundColor = getRandomHexColor();
-      step += 10;
-     divBox.style.width = `"${step}px"`;
-    divBox.style.height = `"${step}px"`;
-    console.log("step",step);
-    boxes.append(divBox)
-    console.log(divBox);
+    divBox.style.width = `${boxSizing}px`;
+    divBox.style.height = `${boxSizing}px`;
+    boxSizing+=10
+    newBoxes.push(divBox)
   };
-  
+    boxes.append(...newBoxes)
   };
 
 function destroyBoxes(){
